50356badc3351a22ec5ce2c56717fb6211236a02
Luks17 <lucasthidavort@gmail.com>
2023-12-18 23:01:47 -0400

    Updated user_song with timestamp

--- tests/client_mock.rs
+++ tests/client_mock.rs
@@ -1,7 +1,40 @@
 use anyhow::Result;
+use serde_json::json;
 
 #[tokio::test]
 #[ignore]
 async fn client_mock() -> Result<()> {
+    let client = httpc_test::new_client(format!("http://localhost:{}", 7717))?;
+
+    client
+        .do_post(
+            "/api/signup",
+            json!({
+            "username": "user",
+            "pwd": "passwd",
+            }),
+        )
+        .await?;
+
+    client
+        .do_post(
+            "/api/login",
+            json!({
+            "username": "user",
+            "pwd": "passwd"
+            }),
+        )
+        .await?;
+
+    client
+        .do_post(
+            "/api/songs",
+            json!({
+                "link": "https://www.youtube.com/watch?v=fJ9rUzIMcZQ"
+                }
+            ),
+        )
+        .await?;
+
     Ok(())
 }
